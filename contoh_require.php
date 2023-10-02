771f3c76f5cc3334acac272f57560f7dd53f24cc
unknown <dilalsm756@gmail.com>
2023-10-02 14:12:29 +0700

    first commit

---
+++ contoh_require.php
@@ -0,0 +1,9 @@
+<?php
+     $a="Saya sedang belajar PHP";
+     function tulistebal($teks)
+     {
+        echo "<b>$teks</b>";
+     }
+     // Memanggil fungsi dan menggunakan variabel
+    tulistebal($a);
+?>
